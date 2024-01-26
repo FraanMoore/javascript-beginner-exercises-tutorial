79092328866ae53b5b5fed963865ff918aa6002c
Francisca Moore <118231279+FraanMoore@users.noreply.github.com>
2024-01-26 01:26:52 +0000

    avance ejercicios

--- exercises/11-Your_first_if/app.js
+++ exercises/11-Your_first_if/app.js
@@ -1,3 +1,13 @@
 let total = prompt('How many km are left to go?');
 
 // Your code below:
+
+if(100 > 0 ){
+    console.log("We still have a bit of driving left to go");
+}
+if(50 > 0) {
+    console.log("We'll be there in 5 minutes");
+}
+if(50 >= 0) {
+    console.log("I'm parking. I'll see you right now")
+};
